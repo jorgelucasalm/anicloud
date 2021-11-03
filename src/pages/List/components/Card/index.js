2644e77ea6e311d708399d2486fcc9213aa5a312
Jorge Lucas <jorge.lucasalm@gmail.com>
2021-11-03 15:32:30 -0300

    refactory(list): refeita a forma como passa as props

--- src/pages/List/components/Card/index.js
+++ src/pages/List/components/Card/index.js
@@ -1,14 +1,14 @@
 import "./style.css";
 
 const Card = (props) => {
+  const consoleTest = () => {};
+
   return (
     <div className="card">
-      <img
-        src={props.children.attributes.posterImage.small}
-        alt=""
-      />
-      {console.log(props.children)}
-      <h2>{props.children.attributes.titles.en !== undefined ? props.children.attributes.titles.en : props.children.attributes.titles.en_jp}</h2>
+      <img src={props.image} alt="" onClick={consoleTest} />
+      <h2>
+        {props.name !== undefined ? props.name : props.nameAlternative}
+      </h2>
     </div>
   );
 };
--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -4,21 +4,40 @@ import "./style.css";
 import api from "../../services/api";
 import { useEffect, useState } from "react";
 
+/*
+useEffect(() => {
+  api.get("").then((response) => {
+    setAnimes(response.data.data);
+  });
+}, []);
+*/
+
 const List = () => {
   const [animes, setAnimes] = useState([]);
 
   useEffect(() => {
     api.get("").then((response) => {
-      setAnimes(response.data.data);
+      const res = response.data.data;
+      let scopeFilter = [];
+      res.map((anime) => {
+        scopeFilter.push({
+          id: anime.id,
+          name: anime.attributes.titles.en,
+          nameAlternative: anime.attributes.titles.en_jp,
+          image: anime.attributes.posterImage.small,
+        });
+      });
+      setAnimes(scopeFilter);
     });
   }, []);
 
   return (
     <>
       <Navbar />
+      {}
       <div className="list">
         {animes.map((anime) => {
-          return <Card key={anime.id}>{anime}</Card>;
+          return <Card key={anime.id} {...anime}/>;
         })}
       </div>
     </>

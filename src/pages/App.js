fe1748b152925547649c82d10366cde24c6bc684
Jorge Lucas <jorge.lucasalm@gmail.com>
2021-11-06 19:31:27 -0300

    faet(list): adiciona as o consumo de anime e manga

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import Navbar from '../components/navbar';
-import List from './List';
 import Routes from '../router';
 
 function App() {
--- src/pages/List/components/Card/index.js
+++ src/pages/List/components/Card/index.js
@@ -1,14 +1,39 @@
+import { useEffect, useState } from "react";
 import "./style.css";
+import api from "../../../../services/api";
 
 const Card = (props) => {
+  const [animeGenre, setAnimeGenre] = useState([]);
+  const [mangaGenre, setMangaGenre] = useState([]);
+
+  useEffect(() => {
+    api.get("/anime/" + props.id + "/genres").then((genre) => {
+      const data = genre.data.data;
+      setAnimeGenre(data[0].attributes.name);
+    });
+    api.get("/manga/" + props.id + "/genres").then((genre) => {
+      const data = genre.data.data;
+      if (data.length !== 0) {
+        setMangaGenre(data[0].attributes.name)
+      } else {
+        return setMangaGenre(NaN);
+      }
+    });
+  }, []);
+
   const consoleTest = () => {};
 
   return (
     <div className="card">
       <img src={props.image} alt="" onClick={consoleTest} />
       <h2>
-        {props.name !== undefined ? props.name : props.nameAlternative}
+        {props.name !== undefined || null ? props.name : props.nameAlternative}
       </h2>
+      <div className="card-status">
+        <span className="genre">{mangaGenre}</span>
+        <span className="date">{props.date}</span>
+        <span className="status">{props.status}</span>
+      </div>
     </div>
   );
 };

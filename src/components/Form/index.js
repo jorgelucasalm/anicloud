31f64a70dc21b3cf7f7cfd56a72a62a8dcd0735b
João Victor A <jvas1999@gmail.com>
2021-11-10 19:19:28 -0300

    tela login
    beta v0.6

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 const Form = styled.form` 
 max-width: 245px;
-margin-right: 5vw;
-margin-bottom: 8vh;
+margin-right: 7vw;
+margin-top: 5vh;
 `;
 
 export default Form;

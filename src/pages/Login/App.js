815c9095bf04c56cf13a065dea4f28dc3fd72772
João Victor A <jvas1999@gmail.com>
2021-11-10 20:41:13 -0300

    list
    beta

--- src/pages/Login/App.js
+++ src/pages/Login/App.js
@@ -1,12 +1,11 @@
 import Main from "./components/Main";
 import FormNew from "./components/FormNew";
-import Reset from "./components/styles/generic/Reset";
+// import Reset from "./components/styles/generic/Reset";
 import Navbar from "../../components/navbar";
 
 function Login() {
   return (
     <>
-      <Reset />
       <Navbar/>
       <Main>
         <FormNew />

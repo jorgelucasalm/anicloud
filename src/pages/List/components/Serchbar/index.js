a5fd59b5cced5d901d054270c186e4872a0539fc
Jorge Lucas <jorge.lucasalm@gmail.com>
2021-11-14 01:43:22 -0300

    feat: adiciona searchbar funcional

--- src/pages/List/components/Serchbar/index.js
+++ src/pages/List/components/Serchbar/index.js
@@ -1,8 +1,16 @@
 import React from "react";
-import './style.css'
+import "./style.css";
+import icon from '../../../../assets/img/search.png';
 
-const Searchbar = () => {
-  return <div>Olá</div>
+const Searchbar = (props) => {
+  return (
+    <div className="searchbar">
+    <label htmlFor="">
+      <img src={icon} alt="Icon" className="icon"/>
+      <input type="text" placeholder="Pesquisar" onChange={props.onChange}/>
+    </label>
+    </div>
+  );
 };
 
 export default Searchbar;

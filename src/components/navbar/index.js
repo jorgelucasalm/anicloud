113b999a36c85988ae77a8a2105ea80a3b6970ff
Jorge Lucas <jorge.lucasalm@gmail.com>
2021-11-17 19:54:00 -0300

    feat: adiciona o nome do site e a logo

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,32 +1,43 @@
 import "./style.css";
 import { Link } from "react-router-dom";
-import {  useRef } from "react";
+import { useRef } from "react";
 
 const Navbar = () => {
   const list = useRef(null);
 
   const onClick = () => {
     const span = list.current; // corresponding DOM node
-    var css = (span.className === "navbar-list active") ? "none" : "active";
+    var css = span.className === "navbar-list active" ? "none" : "active";
     span.className = "navbar-list " + css;
   };
 
   return (
     <nav>
       <div>
-        <span className="navbar-title"><Link to="/">Animes/Manga</Link></span>
+        <span className="navbar-title">
+          <Link to="/">AniCloud</Link>
+        </span>
         <ul className="navbar-list" ref={list}>
-          <Link to="/animes"><li>Animes</li></Link>
-          <Link to="/mangas"><li>Mangás</li></Link>
-          <Link to="/noticias"><li>Notícias</li></Link>
-          <Link to="/login"><li className="hidden">Entrar</li></Link>
+          <Link to="/animes">
+            <li>Animes</li>
+          </Link>
+          <Link to="/mangas">
+            <li>Mangás</li>
+          </Link>
+          <Link to="/noticias">
+            <li>Notícias</li>
+          </Link>
+          <Link to="/login">
+            <li className="hidden">Entrar</li>
+          </Link>
         </ul>
-        <span><Link to="/login">Entrar</Link></span>
+        <span>
+          <Link to="/login">Entrar</Link>
+        </span>
         <button onClick={onClick}>
           <span id="hamburguer"></span>
         </button>
       </div>
-
     </nav>
   );
 };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,10 @@
 import Navbar from "../../components/navbar";
 import "./style.css";
-import Image from "../../assets/img/teste.jpg";
+import Logo from "../../assets/img/logo.png";
 import Jorge from "../../assets/img/Jorge.jfif";
 import Joao from "../../assets/img/Joao.jfif";
 import Ian from "../../assets/img/Ian.jfif";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Home = () => {
   return (
@@ -12,9 +13,9 @@ const Home = () => {
       <section className="home">
         <div className="home-content">
           <div className="text-content">
-            <h1>Bem-vindo ao X!</h1>
+            <h2>Bem-vindo ao aniCloud</h2>
             <p>
-              O (nome do site) é o melhor site onde você pode assistir seus
+              O aniCloud é o melhor site onde você pode assistir seus
               animes favoritos e também ler mangás online, isso mesmo, tudo na
               nossa plataforma. Aqui, você pode registrar uma conta de usuário
               para acessar a coleção de Animes e Mangás e aproveitá-las de forma
@@ -22,7 +23,7 @@ const Home = () => {
             </p>
           </div>
           <div className="image-content i-fe">
-            <img src={Image} alt="" />
+            <img src={Logo} alt="" />
           </div>
         </div>
 
@@ -31,7 +32,7 @@ const Home = () => {
             <img src={Image} alt="" />
           </div>
           <div className="text-content">
-            <h1>O que é Anime?</h1>
+            <h2>O que é Anime?</h2>
             <p>
               Animes e Mangás são desenhos animados e em quadrinhos de origem
               japonesa. No Japão, anime e mangá são nomes dados a qualquer tipo
@@ -42,15 +43,48 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="about-content">
-          <h1>Sobre nós</h1>
+        <section className="about-content">
+          <h2>Sobre nós</h2>
 
-          <div className="image-about-us i-fs">
-            <img src={Jorge} alt="Jorge Lucas" />
-            <img src={Joao} alt="João Victor" />
-            <img src={Ian} alt="" />
+          <div className="about-us-profile">
+            <div className="about-profile">
+              <img src={Jorge} alt="Jorge Lucas" />
+              <h2>Jorge Lucas</h2>
+              <div className="about-link">
+                <a href="/">
+                  <FaGithub size="3rem" />
+                </a>
+                <a href="">
+                  <FaLinkedin  size="3rem"/>
+                </a>
+              </div>
+            </div>
+            <div className="about-profile">
+              <img src={Joao} alt="João Victor" />
+              <h2>João Victor</h2>
+              <div className="about-link">
+                <a href="/">
+                  <FaGithub size="3rem" />
+                </a>
+                <a href="">
+                  <FaLinkedin  size="3rem"/>
+                </a>
+              </div>
+            </div>
+            <div className="about-profile">
+              <img src={Ian} alt="Ian" />
+              <h2>Ian</h2>
+              <div className="about-link">
+                <a href="/">
+                  <FaGithub size="3rem" />
+                </a>
+                <a href="">
+                  <FaLinkedin  size="3rem"/>
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
+        </section>
       </section>
     </>
   );
